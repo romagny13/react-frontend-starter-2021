62569503b5700fe81f5a87947015f5b0783584be
romagny13 <romagny13@yahoo.fr>
2021-08-28 12:51:46 +0200

    @babel/plugin-transform-runtime added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import React, { useEffect, useState } from "react";
 export default function App() {
   const [count, setCount] = useState(0);
 
-  useEffect(() => {
+  useEffect(async () => {
     console.log(process.env.NODE_ENV);
+
+    function testAsync() {
+      return setTimeout(() => {
+        console.log("transform-runtime ok");
+      }, 2000);
+    }
+    await testAsync();
   }, []);
 
   const handleClick = () => {
